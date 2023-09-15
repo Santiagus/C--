da9f4e9e7422e624177dded617c51f9d8c32d5ce
Santiago <santiagoabad@gmail.com>
2023-09-15 17:31:09 +0000

    Add for_each, for_each_n examples

--- 05_Flowcontrol_RangeBasedFor.cpp
+++ 05_Flowcontrol_RangeBasedFor.cpp
@@ -1,6 +1,28 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
- 
+#include <array>
+#include <ranges>
+#include <string_view>
+
+struct P
+{
+    int first;
+    char second;
+    friend std::ostream& operator<<(std::ostream& os, const P& p)
+    {
+        return os << '{' << p.first << ",'" << p.second << "'}";
+    }
+};
+
+auto print = [](std::string_view name, auto const& v)
+{
+    std::cout << name << ": ";
+    for (auto n = v.size(); const auto& e : v)
+        std::cout << e << (--n ? ", " : "\n");
+};
+
+
 int main()
 {
     std::vector<int> v = {0, 1, 2, 3, 4, 5};
@@ -60,4 +82,55 @@ int main()
     // alias declaration as init-statement (C++23)
         std::cout << i << ' ';
     std::cout << '\n';
+
+
+    // FOR EACH
+    // =================================================
+    // #include <algorithm>
+    std::cout << "\nfor each:\n";
+
+    std::vector<int> v2 {3, -4, 2, -8, 15, 267};
+
+    auto print_int = [](const int& n) { std::cout << n << ' '; };
+
+    std::cout << "before:\t";
+    std::for_each(v2.cbegin(), v2.cend(), print_int);
+    std::cout << '\n';
+
+    // increment elements in-place
+    std::for_each(v2.begin(), v2.end(), [](int &n) { n++; });
+
+    std::cout << "after:\t";
+    std::for_each(v2.cbegin(), v2.cend(), print_int);
+    std::cout << '\n';
+
+    struct Sum
+    {
+        void operator()(int n) { sum += n; }
+        int sum {0};
+    };
+
+    // invoke Sum::operator() for each element
+    Sum s1 = std::for_each(v2.cbegin(), v2.cend(), Sum());
+    std::cout << "sum:\t" << s1.sum << '\n';
+
+    // FOR EACH N
+    // =================================================
+    // #include <algorithm>
+
+    std::cout << "\nfor each n:\n";
+    std::array a2 {1, 2, 3, 4, 5};
+    print("a", a2);
+    // Negate first three numbers:
+    std::ranges::for_each_n(a2.begin(), 3, [](auto& n) { n *= -1; });
+    print("a", a2);
+
+    std::array s { P{1,'a'}, P{2, 'b'}, P{3, 'c'}, P{4, 'd'} };
+    print("s", s);
+    // Negate data members 'P::first' using projection:
+    std::ranges::for_each_n(s.begin(), 2, [](auto& x) { x *= -1; }, &P::first);
+    print("s", s);
+    // Capitalize data members 'P::second' using projection:
+    std::ranges::for_each_n(s.begin(), 3, [](auto& c) { c -= 'a'-'A'; }, &P::second);
+    print("s", s);
 }
